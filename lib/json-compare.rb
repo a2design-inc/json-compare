883399d5b44dfb5526ee670200cb1ef51cfa4241
Stanislav Chistenko <skvest1@gmail.com>
2012-09-09 16:35:58 +0700

    Added exclusion param; Added Comparer.excluded_keys property; Added possibility to ignore changes for keys which exists in excluded_keys

--- lib/json-compare.rb
+++ lib/json-compare.rb
@@ -2,8 +2,9 @@ require 'json-compare/version'
 require 'json-compare/comparer'
 
 module JsonCompare
-  def self.get_diff(old, new)
+  def self.get_diff(old, new, exclusion = [])
     comparer = JsonCompare::Comparer.new
+    comparer.excluded_keys = exclusion
     comparer.compare_elements(old,new)
   end
 end
