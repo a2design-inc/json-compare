b86646fb7c9391c41855dea82e8b1f8b9c7febed
Wil Chandler <wilchandler2@gmail.com>
2014-12-03 22:30:25 -0600

    Refactored for readability

--- lib/json-compare/comparer.rb
+++ lib/json-compare/comparer.rb
@@ -12,25 +12,25 @@ module JsonCompare
       if old.kind_of? Hash
         if new.kind_of? Array
           diff_hash = compare_hash_array(old, new)
-        else
+        elsif new.kind_of? Hash
           diff_hash = compare_hashes(old, new)
         end
         diff = diff_hash if diff_hash.count > 0
-      elsif (!is_boolean(old) || !is_boolean(new)) && old.class != new.class
+      elsif !(is_boolean(old) && is_boolean(new)) && old.class != new.class
         diff = new
       elsif old.kind_of? Array
         diff_arr = compare_arrays(old, new)
         diff = diff_arr if diff_arr.count > 0
       else
-        string_diff = compare_strings(old, new)
-        diff = string_diff unless string_diff.nil?
+        diff_string = compare_strings(old, new)
+        diff = diff_string unless diff_string.nil?
       end
       diff
     end
 
     def compare_hashes(old_hash, new_hash)
-      old_hash = old_hash == nil ? {} : old_hash
-      new_hash = new_hash == nil ? {} : new_hash
+      old_hash = {} if old_hash.nil?
+      new_hash = {} if new_hash.nil?
       keys = (old_hash.keys + new_hash.keys).uniq
       result = get_diffs_struct
       keys.each do |k|
@@ -47,30 +47,28 @@ module JsonCompare
           end
         end
       end
+      
       filter_results(result)
     end
 
     def compare_arrays(old_array, new_array)
-      old_array_length = old_array.count
-      new_array_length = new_array.count
-      inters = [old_array.count, new_array.count].min
-
+      old_length = old_array.count
+      new_length = new_array.count
+      min_length = [old_length, new_length].min
       result = get_diffs_struct
 
-      (0..inters).map do |n|
+      (0..min_length).each do |n|
         res = compare_elements(old_array[n], new_array[n])
-        result[:update][n] = res unless (res.nil? || (res.respond_to?(:empty?) && res.empty?))
+        unless (res.nil? || (res.respond_to?(:empty?) && res.empty?))
+          result[:update][n] = res
+        end
       end
 
       # the rest of the larger array
-      if inters == old_array_length
-        (inters..new_array_length).each do |n|
-          result[:append][n] = new_array[n]
-        end
+      if min_length == old_length
+        (min_length..new_length).each { |n| result[:append][n] = new_array[n] }
       else
-        (inters..old_array_length).each do |n|
-          result[:remove][n] = old_array[n]
-        end
+        (min_length..old_length).each { |n| result[:remove][n] = old_array[n] }
       end
 
       filter_results(result)
@@ -79,11 +77,11 @@ module JsonCompare
     def compare_hash_array(old_hash, new_array)
       result = get_diffs_struct
 
-      (0..new_array.count).map do |n|
+      (0..new_array.count).each do |n|
         next if new_array[n].nil?
         if n == 0
           res = compare_elements(old_hash, new_array[0])
-          result[:update][n] = res unless res.empty?
+          result[:update][n] = res unless res.respond_to?(:empty?) && res.empty?
         else
           result[:append][n] = new_array[n]
         end
@@ -93,7 +91,7 @@ module JsonCompare
     end
 
     def compare_strings(old_string, new_string)
-      (old_string != new_string) ? new_string.to_s : ""
+      old_string == new_string ? "" : new_string.to_s
     end
 
     # Returns diffs-hash with bare structure
@@ -103,18 +101,15 @@ module JsonCompare
 
     def filter_results(result)
       return {} if result.nil?
-      out_result = {}
-      result.each_key do |change_type|
-        next if result[change_type].nil?
-        temp_hash = {}
-        result[change_type].each_key do |key|
-          next if result[change_type][key].nil?
-          next if @excluded_keys.include? key
-          temp_hash[key] = result[change_type][key]
+      filtered_hash = {}
+      result.each do |change_type, changes|
+        next if changes.nil?
+        keep = changes.reject do |key, val|
+          val.nil? || (@excluded_keys.include? key)
         end
-        out_result[change_type] = temp_hash if temp_hash.count > 0
+        filtered_hash[change_type] = keep unless keep.empty?
       end
-      out_result
+      filtered_hash
     end
   end
 end
